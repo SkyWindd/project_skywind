063ebc6f8054dfef19816a0fd199fcf8b4b0c21a
Gia Hưng <ngogiahung.hcm@gmail.com>
2025-10-13 10:30:52 +0700

    Gộp code MainLayout (BreadcrumbNav + ChatBox)

--- frontend/src/layout/mainlayout.jsx
+++ frontend/src/layout/mainlayout.jsx
@@ -1,26 +1,35 @@
 import { Outlet } from "react-router-dom";
-import Footer from '@/layout/footer'
-import Header from '@/layout/header'
-import BreadcrumbNav from "@/layout/breadcrumb"
-import React from 'react'
-import Header_t from './header'
+import Footer from '@/layout/footer';
+import Header from '@/layout/header';
+import BreadcrumbNav from "@/layout/breadcrumb";
+import React from 'react';
+import ChatBox from '../chatbox';
 
 function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header dính trên đầu khi cuộn */}
+      {/* Header cố định trên đầu */}
       <header>
         <Header />
       </header>
 
-     
+      {/* Thanh breadcrumb */}
+      <BreadcrumbNav />
+
+      {/* Nội dung chính */}
+      <main className="flex-grow bg-secondary">
+        <Outlet /> {/* nơi render Home, Laptop,... */}
+      </main>
+
+      {/* Chatbox hiển thị ở góc phải dưới */}
+      <ChatBox />
 
       {/* Footer luôn ở cuối trang */}
       <footer>
         <Footer />
       </footer>
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
